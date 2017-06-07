41dae9fb030a21851af4e737b6ce6a21ad21804c
pratamaaudi <pratamaaudi@gmail.com>
2017-06-07 08:21:46 +0700

    UPDATE
    - link login to signup
    - animation

--- app/src/main/java/com/example/audi/uaspenir/Login.java
+++ app/src/main/java/com/example/audi/uaspenir/Login.java
@@ -1,9 +1,13 @@
 package com.example.audi.uaspenir;
 
+import android.content.Intent;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.transition.Transition;
 import android.transition.TransitionInflater;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -12,7 +16,17 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Transition a = TransitionInflater.from(this).inflateTransition(R.transition.slide_right);
+        Transition a = TransitionInflater.from(this).inflateTransition(R.transition.slide_bottom);
         getWindow().setEnterTransition(a);
+    }
+
+    public void daftar(View view){
+        Intent i = new Intent(this, SignUp.class);
+        Pair<View, String> p1 = Pair.create(findViewById(R.id.imglogo), "imglogo");
+        Pair<View, String> p2 = Pair.create(findViewById(R.id.link_signup), "txtjudul");
+        Pair<View, String> p3 = Pair.create(findViewById(R.id.input_password), "password");
+        Pair<View, String> p4 = Pair.create(findViewById(R.id.btn_login), "button");
+        ActivityOptionsCompat option = ActivityOptionsCompat.makeSceneTransitionAnimation(this, p1, p2, p3, p4);
+        startActivity(i, option.toBundle());
     }
 }
--- app/src/main/java/com/example/audi/uaspenir/SignUp.java
+++ app/src/main/java/com/example/audi/uaspenir/SignUp.java
@@ -2,6 +2,8 @@ package com.example.audi.uaspenir;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
 
 public class SignUp extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Transition a = TransitionInflater.from(this).inflateTransition(R.transition.slide_up);
+        getWindow().setEnterTransition(a);
     }
 }
